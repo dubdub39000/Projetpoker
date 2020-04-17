a221f7e04f2d49f1f2776db730983c350fb23a74
dubdub39000 <maillyjeremy69@gmail.com>
2020-04-17 14:42:56 +0200

    implémentation getrang version 1

--- main.c
+++ main.c
@@ -37,7 +37,8 @@ typedef struct joueurs {
 }joueurs;
 
 mainjoueur tirage;
-
+carte cartes;
+char values[]={'a', 'k', 'q', 'j', 't','9', '8', '7', '6', '5', '4', '3', '2'};
 /***************************************************Définitions***************************************************/
 
 carte  generatecard ();
@@ -45,19 +46,18 @@ mainjoueur generatemain ();
 void affichermain(mainjoueur);
 bool is_same_figure(carte *, carte *);
 bool is_same_valeur(carte *, carte *);
+int  getrang(carte );
 /*************************************************Fonctions générations main***************************************/
 carte  generatecard() {
     unsigned long seed = clock()+time(NULL)+getpid();
     srand(seed);
     sleep(1);                                                                                                           //timer qui permet de laisser le temps au rand de se réinit.
-    char values[]={'a', 'k', 'q', 'j', 't','9', '8', '7', '6', '5', '4', '3', '2'};
     char figures[]={'H', 'D', 'C', 'S'};
-    carte cartes;
     int nbr1 = rand() % 13;
     int nbr2 = rand() % 4;
     cartes.valeur = values[nbr1];
     cartes.figure = figures[nbr2];
-    //printf("carte aleatoire : %c%c\n", cartes.valeur, cartes.figure);                                             //test de bon fonctionnement
+    printf("carte aleatoire : %c%c\n", cartes.valeur, cartes.figure);                                                 //test de bon fonctionnement
     return cartes;
 }
 mainjoueur generatemain() {
@@ -101,11 +101,23 @@ bool is_same_valeur(carte * carte1, carte * carte2) {                           
     return carte1->valeur == carte2->valeur;
 }
 
+/*************************************************fonction force et tri de la main******************************/
+int getrang(carte  cartes) {
+    int a;
+    for (int i = 0; i < 13; ++i) {
+        if (cartes.valeur==values[i])
+        a=i;
+    }
+    return a;
+}
+
 /***************************************************code********************************************************/
 int main() {
     //for (int i = 0; i < 5; ++i) {}                                                                                     //boucle test pour tester les doublons
-        generatemain();
-        affichermain(tirage);
+        //generatemain();
+        //affichermain(tirage);
+        generatecard();
+getrang(cartes);
 
     return 0;
 }
